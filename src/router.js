637df8eb82a7f1e63df3a9ed2356e00834c9519f
tianlishu <tianlishu@vld-tech.com>
2019-09-30 09:56:27 +0800

    日历插件

--- src/router.js
+++ src/router.js
@@ -4,11 +4,12 @@ import Router from 'vue-router'
 Vue.use(Router);
 
 import Demo from './views/demos/demo'
+import Calendar from './views/calendar/index'
 
 export default new Router({
     routes:[
-        {path:'/demo',component:Demo,name:'demo'}
-
+        {path:'/demo',component:Demo,name:'demo'},
+        {path:'/calendar',component:Calendar,name:'calendar' }
     ]
 })
 
